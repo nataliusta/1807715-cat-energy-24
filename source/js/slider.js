9391e7a6cae289b652afba96941ca040bad207db
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-01-20 23:08:51 +0300

    Добавит стилизацию slider

--- source/js/slider.js
+++
@@ -1,6 +0,0 @@
-let sliderRange = document.querySelector(".slider__range");
-let value = 0;
-sliderRange.addEventListener("input", () => {
-  value = sliderRange.value
-  document.querySelector(".slider__image-foreground").style.width = `${value}%`
-});
